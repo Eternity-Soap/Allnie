e0d546046c461aa3e8116adccd5ee815e576d64c
Stosh <schyczewski@gmail.com>
2018-01-27 18:59:44 -0500

    Additional development towards Alpha CLI version.

--- src/com/soap/HardcodedCoins.java
+++ src/com/soap/HardcodedCoins.java
@@ -2,6 +2,7 @@ package com.soap;
 
 public abstract interface HardcodedCoins 
 {
+	//When adding new hardcoded coin, remember to tweak remove method in main.
 	public static final Coin BTC = new Coin((byte)0x00, (byte)0x80, "Bitcoin", "BTC");
 	public static final Coin LTC = new Coin((byte)0x30, (byte)0xb0, "Litecoin", "LTC");
 	public static final Coin BCH = new Coin((byte)0x00, (byte)0x80, "Bitcoin Cash", "BCH");
--- src/com/soap/Main.java
+++ src/com/soap/Main.java
@@ -6,75 +6,62 @@ import java.util.Scanner;
 
 public class Main 
 {
-	public static void main(String[] args)
-	{	
-		/* Scanner in = new Scanner(System.in);
-		FileUtils.initCoinsFile();
-		List<Coin> list = loadHardcoded();
-		if (FileUtils.loadCoins() != null)
-			list.addAll(FileUtils.loadCoins());
-		list = delDuplicates(list);
-		System.out.print("Add public prefix (in hex): ");
-		String pubprefix;
-		String privprefix;
-		String name;
-		String code;
-		pubprefix = in.nextLine();
-		System.out.print("Add private prefix (in hex): ");
-		privprefix = in.nextLine();
-		System.out.print("Add name: ");
-		name = in.nextLine();
-		System.out.println("Add code: ");
-		code = in.nextLine();
-		Coin coin = new Coin((byte)Integer.parseInt(pubprefix, 16), (byte)Integer.parseInt(privprefix, 16), name, code);
-		list.add(coin);
-		list = delDuplicates(list);
-		try
-		{
-			SatoshiKeypair pair;
-			for (Coin c : list)
-			{
-				pair = new SatoshiKeypair(c);
-				System.out.println(c.NAME);
-				System.out.println(pair.getBase58());
-				System.out.println(pair.getWiF());
-			}
-		}
-		catch (Exception e)
-		{
-			System.out.println(e.getMessage());
-		} 
-		FileUtils.saveCoins(list);
-		System.out.println("done");
-		in.close(); */
-		boolean run = true;
+	static List<Coin> coinlist;
+	static List<SatoshiKeypair> keylist;
+	static Scanner in;
+	static
+	{
 		FileUtils.initFiles();
-		List<Coin> coinlist = loadHardcoded();
-		List<SatoshiKeypair> keylist = new ArrayList<SatoshiKeypair>();
+		keylist = new ArrayList<SatoshiKeypair>();
+		coinlist = loadHardcoded();
 		if (FileUtils.loadCoins() != null)
 			coinlist.addAll(FileUtils.loadCoins());
-		coinlist = delDuplicates(coinlist);
-		printCoins(coinlist);
 		if (FileUtils.loadKeys() != null)
 			keylist = FileUtils.loadKeys();
 		coinlist = delDuplicates(coinlist);
-		while (run)
+		in = new Scanner(System.in);
+	}
+	
+	public static void main(String[] args)
+	{	
+		
+		boolean run = true;
+		String choice = "";
+		//printCoins(coinlist);
+		System.out.println("Welcome to Allnie!");
+		while (true)
 		{
-			System.out.println("Welcome to Allnie!");
 			System.out.println("Please make a selection: ");
 			System.out.println("a: List addresses");
 			System.out.println("b: Add new address");
-			System.out.println("c: Remove address");
+			System.out.println("c: Remove address (keypair will be deleted)");
 			System.out.println("d: Add coin");
-			System.out.println("e: Remove coin");
-			run = false;
+			System.out.println("e: List coins");
+			System.out.println("f: Remove coin");
+			System.out.println("x: Exit");
+			choice = in.nextLine();
+			if (choice.equalsIgnoreCase("a"))
+				listAddrs();
+			else if (choice.equalsIgnoreCase("b"))
+			{
+				addAddr();
+				System.out.println("New address created.");
+			}
+			else if (choice.equalsIgnoreCase("c"))
+				removeAddr();
+			else if (choice.equalsIgnoreCase("d"))
+				addCoin();
+			else if (choice.equalsIgnoreCase("e"))
+				printCoins();
+			else if (choice.equalsIgnoreCase("f"))
+				removeCoin();
+			else if (choice.equalsIgnoreCase("x"))
+			{
+				System.out.println("Halting...");
+				break;
+			}
 		}
-		System.out.println("Printing test key...");
-		System.out.println(keylist.get(0).getCoinname());
-		System.out.println(keylist.get(0).getBase58());
-		System.out.println(keylist.get(0).getWiF());
-		//keylist.add(new SatoshiKeypair(coinlist.get(0)));
-		//System.out.println("New test key added");
+		//in.close();
 		FileUtils.saveCoins(coinlist);
 		FileUtils.saveKeys(keylist);
 	}
@@ -110,12 +97,162 @@ public class Main
 		return false;
 	}
 	
-	public static void printCoins(List<Coin> list)
+	public static void printCoins()
 	{
-		for (Coin c : list)
+		int i = 1;
+		for (Coin c : coinlist)
 		{
-			System.out.println(c.NAME);
+			System.out.println(i + ": " + c.NAME);
+			i++;
 		}
+	}
+	
+	public static void listAddrs()
+	{
+		int choice = 0;
+		String input;
+		System.out.println("For which coin would you like to see addresses?");
+		for (int i = 0; i < coinlist.size(); i++)
+		{
+			System.out.println((i + 1)+" "+coinlist.get(i).NAME);
+		}
+		
+		input = in.nextLine();
+		choice = Integer.parseInt(input);
+		choice--;
+		int i = 1;
+		for (SatoshiKeypair k : keylist)
+		{
+			if (k.getCoinname().equals(coinlist.get(choice).NAME))
+			{
+				System.out.println(i + ": " + k.getBase58());
+				i++;
+			}
+		}
+		if (i == 1)
+			System.out.println("No addresses of this type.");
+	}
+	
+	public static void removeAddr()
+	{
+		int coinchoice = 0;
+		int keychoice = 0;
+		int firstIndex = 0;
+		String input;
+		System.out.println("What kind of address would you like to remove?");
+		for (int i = 0; i < coinlist.size(); i++)
+		{
+			System.out.println((i + 1)+" "+coinlist.get(i).NAME);
+		}
+		
+		input = in.nextLine();
+		coinchoice = Integer.parseInt(input);
+		coinchoice--;
+		for (int i = 0; i < keylist.size(); i++)
+		{
+			if (keylist.get(i).getCoinname().equals(coinlist.get(coinchoice).NAME))
+			{
+				firstIndex = i;
+				break;
+			}
+		}
+		keychoice = firstIndex;
+		System.out.println("Which address would you like to remove?");
+		int i = 0;
+		for (i = firstIndex; keylist.get(i).getCoinname().equals(coinlist.get(coinchoice).NAME); i++)
+		{
+			System.out.println(i + 1 - firstIndex + ": " + keylist.get(i).getBase58());
+		}
+		System.out.println(i + 1 - firstIndex + ": I've changed my mind.");
+		input = in.nextLine();
+		keychoice = Integer.parseInt(input);
+		keychoice = keychoice - 1 + firstIndex;
+		if (keychoice + 1 == i)
+		{
+			System.out.println("Key remove aborted.");
+			return;
+		}
+		keylist.remove(keychoice);
+		System.out.println("Key deleted.");
+	}
+	
+	public static void addAddr()
+	{
+		int choice = 0;
+		String input;
+		System.out.println("What kind of address do you want to make?");
+		for (int i = 0; i < coinlist.size(); i++)
+		{
+			System.out.println((i + 1)+" "+coinlist.get(i).NAME);
+		}
+		
+		input = in.nextLine();
+		choice = Integer.parseInt(input);
+		choice--;
+		if (keylist.size() == 0)
+		{
+			keylist.add(new SatoshiKeypair(coinlist.get(choice)));
+			return;
+		}
+		for (int i = 0; i < keylist.size(); i++)
+		{
+			if (i == keylist.size()-1)
+			{
+				keylist.add(new SatoshiKeypair(coinlist.get(choice)));
+				return;
+			}
+			if (!keylist.get(i).getCoinname().equals(keylist.get(i+1).getCoinname()))
+			{
+				keylist.add(i+1, new SatoshiKeypair(coinlist.get(choice)));
+				return;
+			}
+		}
+	}
+	
+	public static void addCoin()
+	{
+		
+		String name = "";
+		String code = "";
+		String input;
+		byte pubprefix = 0x0;
+		byte privprefix = 0x0;
+		System.out.println("What is the name of the new coin?");
+		name = in.nextLine();
+		System.out.println("What is the currency code (i.e. BTC, LTC...)?");
+		code = in.nextLine();
+		System.out.println("In hexadecimal (do not include 0x), what is the public prefix for this coin (IT IS VERY IMPORTANT THAT THIS IS CORRECT)?");
+		input = in.nextLine();
+		pubprefix = (byte)Integer.parseInt(input, 16);
+		System.out.println("In hexadecimal (do not include 0x), what is the private prefix for this coin (IT IS VERY IMPORTANT THAT THIS IS CORRECT)?");
+		input = in.nextLine();
+		privprefix = (byte)Integer.parseInt(input, 16);
+		coinlist.add(new Coin(pubprefix, privprefix, name, code));
+		System.out.println("New coin successfully added.");
+	}
+	
+	public static void removeCoin()
+	{
+		
+		int choice = 0;
+		int i = 0;
+		String input;
+		System.out.println("Which coin would you like to remove?");
+		for (i = 4; i < coinlist.size(); i++)
+		{
+			System.out.println(i - 3 + ": " + coinlist.get(i).NAME);
+		}
+		System.out.println(i - 3 + ": I've changed my mind.");
+		input = in.nextLine();
+		choice = Integer.parseInt(input);
+		choice += 3;
+		if (choice == i)
+		{
+			System.out.println("Coin remove aborted.");
+			return;
+		}
+		coinlist.remove(choice);
+		System.out.println("Coin removed.");
 	}
 }
 
